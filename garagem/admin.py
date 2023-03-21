8175333d3e59da6eb8e2e8bb7b47eea821c47ebd
Brobss <gabrieldomingos13@gmail.com>
2023-03-21 15:14:25 -0300

    Adição do modelo Cor, Acessorio e Veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Marca
 from .models import Categoria
+from .models import Cor
+from .models import Acessorio
+from .models import Veiculo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
+admin.site.register(Veiculo)
