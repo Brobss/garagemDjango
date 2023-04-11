dd35827a0e556ec00a712907017682b84a5209d9
Brobss <gabrieldomingos13@gmail.com>
2023-04-11 14:57:17 -0300

    Criação Django Rest Framework

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Marca
+from garagem.serializers import MarcaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
