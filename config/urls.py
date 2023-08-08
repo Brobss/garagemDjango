4999d308769e1fd7261ab9807ea394baae795bee
Brobss <gabrieldomingos13@gmail.com>
2023-08-08 12:58:54 -0300

    SimpleJWT

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, VeiculoViewSet
+from garagem.views import (
+    MarcaViewSet,
+    CategoriaViewSet,
+    CorViewSet,
+    AcessorioViewSet,
+    VeiculoViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
@@ -15,4 +25,6 @@ router.register(r"veiculos", VeiculoViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- garagem/views.py
+++ garagem/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from garagem.models import Marca, Categoria, Cor, Acessorio, Veiculo
 from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, AcessorioSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
